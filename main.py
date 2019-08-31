eede529bb2e3309bda9944b31e40adfdf1d524a7
Eric Daugherty <ericdaugherty@Erics-Air.hitronhub.home>
2019-08-31 15:42:33 -0400

    All done

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hi, my name is Eric Daugherty.')
+print('My favorite game is "Rainbow Six Siege, but I am a big fan of the innovation released in games during the 2000s.')
+print('I am concerned that there will be a lot that I do not understand and I will fall behind.')
+print('I am exited to learn how video games run in the first place. The whole concept is so foreign to me.')
+print('Though you said it may be in the ten thousands, my account number for stack overflow wound up being 11980896 lol.')
+print('My github link is https://github.com/eddaughe/01-Introduction')
